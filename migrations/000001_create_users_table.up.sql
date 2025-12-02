427317e6430af67d3286f463a670ff428a74e69a
ariekas <ariariekas@gmail.com>
2025-12-02 09:43:52 +0700

    feat: update migration database

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(225),
     email VARCHAR(100),
-    password VARCHAR(8),
+    password VARCHAR(100),
+    role VARCHAR DEFAULT 'user',
     created_at DATE,
     updated_at DATE
 );
