3cd10dd31328ca582f9c004bd00fd508f529bf2e
ariekas <ariariekas@gmail.com>
2025-12-02 15:07:16 +0700

    feat: create model short link

---
+++ internal/models/shortlink.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type ShortLink struct{
+	Id int `json:"id"`
+	UserId int `json:"userId"`
+	OriginalUrl string `json:"originalUrl"`
+	ShortUrl string `json:"shortUrl"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time	`json:"updatedAt"`
+}
