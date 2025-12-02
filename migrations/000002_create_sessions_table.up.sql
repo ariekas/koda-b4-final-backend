42574cdf4755cb28aecf9d4776ba8d7ab706ac1d
ariekas <ariariekas@gmail.com>
2025-12-02 11:34:04 +0700

    feat: update database migration

--- migrations/000002_create_sessions_table.up.sql
+++ migrations/000002_create_sessions_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
     userId INT NOT NULL REFERENCES users(id),
     refreshToken TEXT,
+    revoked BOOLEAN,
     created_at DATE,
     expires_at DATE, 
     updated_at DATE
